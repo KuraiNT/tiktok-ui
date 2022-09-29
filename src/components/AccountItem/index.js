7fe43097cfcc65ee5c7080ce2ad1c1bef87e3f0f
KuraiNT <89431659+KuraiNT@users.noreply.github.com>
2022-09-29 16:57:05 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,24 +2,22 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
-                src="https://i.pinimg.com/564x/88/58/50/88585036b7294a4b4aa4eca2e4c8a485.jpg"
+                src="shttps://i.pinimg.com/564x/88/58/50/88585036b7294a4b4aa4eca2e4c8a485.jpg"
                 alt="Hoa"
             />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyen Van A</span>
-                    <FontAwesomeIcon
-                        className={cx('check')}
-                        icon={faCheckCircle}
-                    ></FontAwesomeIcon>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}></FontAwesomeIcon>
                 </h4>
                 <span className={cx('username')}>nguyenvana</span>
             </div>
